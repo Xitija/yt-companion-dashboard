6af6f54689f2e362a34a8cbb165be9b5c720aacd
Kshitija Kadam <65657373+Xitija@users.noreply.github.com>
2025-05-18 17:58:16 +0530

    refactor: simplify CSS styles and restructure main component for better readability

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import './index.css';
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
+import { DataProvider } from "./context/VideoContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <DataProvider>
+        <App />
+      </DataProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
